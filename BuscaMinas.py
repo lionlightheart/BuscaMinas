f16acbb694908fa742e3b66cdb0d871287fff783
lionl <lionlightheart@gmail.com>
2022-01-27 16:34:07 +0100

    Este es el bueno Ramon del mañana

--- BuscaMinas.py
+++ BuscaMinas.py
@@ -10,27 +10,52 @@ import random
 
 
 def data_input():
+    DATOS_CORRECTOS = False
     data = 0
-    minimo = 3
-    maximo = 10
+    MINIMO = 3
+    MAXIMO = 10
     data = int(input())
-    if data >= minimo and data <= maximo:
-        return data
-    else:
-        jugar_buscaminas()
+
+    return data
 
 
 def generar_tablero():
+    TABLERO_CORRECTO = False
+    MINIMO = 3
+    MAXIMO = 10
     eje_x = 0
     eje_y = 0
     contador = 0
     contador1 = 0
     tablero = []
-
     print("Introduce el ancho: ", end="")
     eje_x = data_input()
     print("Introduce el alto: ", end="")
     eje_y = data_input()
+    while TABLERO_CORRECTO == False:
+        if eje_x < MINIMO or eje_y < MINIMO:
+            print("Introduce el ancho: ", end="")
+            eje_x = data_input()
+            print("Introduce el alto: ", end="")
+            eje_y = data_input()
+        elif eje_x > MAXIMO or eje_y > MAXIMO:
+            print("Introduce el ancho: ", end="")
+            eje_x = data_input()
+            print("Introduce el alto: ", end="")
+            eje_y = data_input()
+        elif eje_x < MINIMO or eje_y > MAXIMO:
+            print("Introduce el ancho: ", end="")
+            eje_x = data_input()
+            print("Introduce el alto: ", end="")
+            eje_y = data_input()
+        elif eje_x > MAXIMO or eje_y < MINIMO:
+            print("Introduce el ancho: ", end="")
+            eje_x = data_input()
+            print("Introduce el alto: ", end="")
+            eje_y = data_input()
+        else:
+            TABLERO_CORRECTO = True
+
     print(f"{eje_x}, {eje_y}") # Borrar
     tablero = [["x " for x in range(eje_x)] for y in range(eje_y)]
 
@@ -45,17 +70,18 @@ def mostar_tablero(tablero_puro, eje_x, eje_y):
     for contador in range(eje_y):
         print(contador + 1, end=" ")
         for contador1 in range(eje_x):
-            print(f"{tablero_puro[contador][contador1]}", end="")
+            if tablero_puro[contador][contador1] == "m ":
+                print("x ", end="")
+            else:
+                print(f"{tablero_puro[contador][contador1]}", end="")
         print("")
 
 
-
-def colocar_minas(minas, eje_x, eje_y):
-    tablero_con_minas = [["x " for x in range(eje_x)] for y in range(eje_y)]
+def colocar_minas(minas, eje_x, eje_y, tablero_con_minas):
     mina_grafica = "m "
     random_x = 0
     random_y = 0
-
+    print("\n" * 20)
     random_x = random.randrange(eje_x - 1)
     random_y = random.randrange(eje_y - 1)
     for _ in range(minas):
@@ -79,10 +105,9 @@ def generar_minas(casillas):
     return catidad_de_minas
 
 
-def comprobar_datos(tablero, tablero_con_minas, eje_x, eje_y):
+def comprobar_datos( tablero_con_minas, eje_x, eje_y, vida, minas):
     dato_x = 0
     dato_y = 0
-    vida = True
     mina = "m "
 
     print("Introduce el eje y:", end="")
@@ -91,32 +116,36 @@ def comprobar_datos(tablero, tablero_con_minas, eje_x, eje_y):
     dato_y = data_input() - 1
     if dato_x in range(eje_x) and dato_y in range(eje_y):
         if tablero_con_minas[dato_x][dato_y] == mina:
-            tablero[dato_x][dato_y] = mina
-            vida = True
-            return tablero, vida
+            tablero_con_minas[dato_x][dato_y] = "V "
+            minas -= 1
+            return tablero_con_minas, vida, minas
         else:
-            print("perdiste")
-            vida = False
-            return tablero, vida
+            vida -= 1
+            print("Una vida menos")
+            print(f"cantidad de vidas {vida}")
+            return tablero_con_minas, vida, minas
     else:
         print("Sos re bobo")
-    return tablero, vida
+    return tablero_con_minas, vida
 
 
 def jugar_buscaminas():
     casillas = 0
-    vida = True
+    vida = 0
     verdadero_faslo = True
     tablero = []
 
     tablero, eje_x, eje_y = generar_tablero()
     casillas = eje_x * eje_y
     minas = generar_minas(casillas)
-    tablero_con_minas = colocar_minas(minas, eje_x, eje_y)
+    tablero_con_minas = colocar_minas(minas, eje_x, eje_y, tablero)
     mostar_tablero(tablero_con_minas, eje_x, eje_y)
-    while vida == verdadero_faslo and tablero != tablero_con_minas:
-        tablero, vida = comprobar_datos(tablero, tablero_con_minas, eje_x, eje_y)
+    vida = minas * 2
+    print(f"cantidad de vidas {vida}")
+    while vida != 0 and minas != 0:
+        tablero, vida, minas = comprobar_datos(tablero_con_minas, eje_x, eje_y, vida, minas)
         mostar_tablero(tablero, eje_x, eje_y)
+    print("Fin del juego")
 
 
 jugar_buscaminas()
